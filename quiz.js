6fd88ac0a2a51e5c551188222e12431a2234315e
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-21 15:31:14 -0600

    reset code base, inserted select menus, instantiat objects and set their values to user input and selection

--- quiz.js
+++ quiz.js
@@ -1,54 +1,28 @@
 // global variables
 
+var combatant1 = new Battledome.RobotGuild.BotClass
+var combatant2 = new Battledome.RobotGuild.BotClass
+var userName1;
+var userName2;
 
 // When your user interface first loads,
 //--provide 2 text inputs to name the two robots that will do battle.
 // You must also provide a select element underneath each text input
 //--so that the user can select one of the 6 robot models you defined.
 
-$(document).ready(function() {
-    /*
-    Show the initial view that accepts robot names
-    */
-    $("#name-robot").show();
 
-    /*
-    When any button with card__link class is clicked,
-    move on to the next view.
-    */
-    $(".card__link").click(function(e) {
-        var nextCard = $(this).attr("next");
-        var moveAlong = false;
 
-        switch (nextCard) {
-            case "card--class":
-                moveAlong = ($("#player-name").val() !== "");
-                warrior.playerName = $("#player-name").val();
-                // console.log(warrior);
-                break;
+$("#player-1-ready").click(function(e){
+    console.log($(".p1-select-bot option:selected").text());
+    combatant1.name = $('#player-one-name').val();
+    console.log(combatant1)
+})
 
-            case "card--weapon":
-                moveAlong = ($("#player-name").val() !== "");
-
-                warrior.class = new Gauntlet.GuildHall[selectedClass]
-                // console.log(warrior);
-                break;
-
-            case "card--battleground":
-                moveAlong = ($("#player-name").val() !== "");
-                warrior.weapon = new Gauntlet.Armory[selectedWeapon]
-                console.log(warrior);
-                displayStats();
-                break;
-        }
-
-        if (moveAlong) {
-          $(".card").hide();
-          $("." + nextCard).show();
-        }
-    });
-});
-
+$("#player-2-ready").click(function(e){
+    console.log($(".p2-select-bot option:selected").text());
+    combatant2.name = $('#player-two-name').val()
+    console.log(combatant2);
+})
 
 // Provide a Attack! button that, when clicked, simply applies the damage output of each robot against the other one.
 // Once either robot's health is <0 display a message that the battle is over, and which one won. For example...
