f50e6504ad1757c46e07bc7e13f9efed5b7d092f
Luke Woodward <luke.wdwrd@gmail.com>
2016-12-21 16:34:56 -0600

    re-organized

--- quiz.js
+++ quiz.js
@@ -1,27 +1,36 @@
 // global variables
-
-var combatant1 = new Battledome.RobotGuild.BotClass
-var combatant2 = new Battledome.RobotGuild.BotClass
+var typeSelect;
+var combatant1;
+var combatant2;
 var userName1;
 var userName2;
 
-// When your user interface first loads,
-//--provide 2 text inputs to name the two robots that will do battle.
-// You must also provide a select element underneath each text input
-//--so that the user can select one of the 6 robot models you defined.
 
-
+//   When your user interface first loads,
+//    provide 2 text inputs to name the two robots that will do battle.
+//  You must also provide a select element underneath each text input
+//    so that the user can select one of the 6 robot models you defined.
 
 $("#player-1-ready").click(function(e){
-    console.log($(".p1-select-bot option:selected").text());
-    combatant1.name = $('#player-one-name').val();
-    console.log(combatant1)
+
+    typeSelect = $(".p1-select-bot option:selected").text();
+
+    userName1 = $('#player-one-name').val();
+
+    combatant1 = new Battledome.RobotGuild[typeSelect](userName1);
+
+    console.log(combatant1);
 })
 
 $("#player-2-ready").click(function(e){
-    console.log($(".p2-select-bot option:selected").text());
-    combatant2.name = $('#player-two-name').val()
-    console.log(combatant2);
+
+    typeSelect = $(".p2-select-bot option:selected").text();
+
+    userName2 = $('#player-two-name').val();
+
+    combatant2 = new Battledome.RobotGuild[typeSelect](userName2);
+    console.log(combatant2)
+
 })
 
 // Provide a Attack! button that, when clicked, simply applies the damage output of each robot against the other one.
